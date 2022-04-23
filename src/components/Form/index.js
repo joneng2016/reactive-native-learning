a655837c6fb68ce0f30b0602fd92b9609f9187c3
Jonathan Morris Samara <jonathan_samara@hotmail.com>
2022-04-23 19:55:02 -0300

    feito a parte de lógica funcional do app

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,19 +1,59 @@
-import React from 'react' 
-import {TextInput, Text, View} from 'react-native'
+import React, { useState, onPress} from 'react' 
+import {TextInput, Text, View, Button} from 'react-native'
+import Title from '../Title'
+import ResultImc from '../ResultImc';
 
 export default function Form() {
+    
+    const [height, setHeight] = useState(null)
+    const [weight, setWeight] = useState(null)
+    const [messageImc, setMessageImc] = useState("preencha o peso e altura")
+    const [imc,setImc] = useState(null)
+    const [textButton, setTextButton] = useState("Calcular")
+
+    function imcCalculator() {
+        return setImc(weight/(height*height).toFixed(2))
+    }
+
+    function validationImc() {
+        if (weight !== null && height !== null) {
+            imcCalculator()
+            setHeight(null)
+            setWeight(null)
+            setMessageImc("Seu imc é igual: ")
+            setTextButton("Calcular Novamente")
+            return
+        }
+        setImc(null)
+        setTextButton("Calcular")
+        setMessageImc("preencha o peso e a altura")
+    }
+    
     return(
     <View>            
         <View>
+            <Title/>
             <Text>Altura</Text>
             <TextInput
+                value={height}
+                onChangeText={setHeight}
                 placeholder="Ex. 1.75"
-                KeybordType="numeric"
+                keyboardType="numeric"
             />
             <Text>Peso</Text>
             <TextInput
+                value={weight}
+                onChangeText={setWeight}
                 placeholder="Ex. 75.365"
-                KeybordType="numeric"
+                keyboardType="numeric"
+            />
+            <Button
+                onPress={() => validationImc()}
+                title={textButton}
+            />
+            <ResultImc
+                messageResultImc={messageImc}
+                resultImc={imc}
             />
         </View>
     </View>
